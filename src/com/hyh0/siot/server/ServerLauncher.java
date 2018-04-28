3f15ef8b6ea169a5d7b9620f90b5ed20af9161da
heyuhang0 <heyuhang0@outlook.com>
2018-04-28 13:48:18 +0800

    保持进度 1. 合并了fstcpsocket模块处理tcp连接

--- src/com/hyh0/siot/server/ServerLauncher.java
+++ src/com/hyh0/siot/server/ServerLauncher.java
@@ -1,15 +1,31 @@
 package com.hyh0.siot.server;
 
+import com.hyh0.fstcpsocket.server.TCPServer;
+import com.hyh0.siot.server.Data.DataBase;
+
 public class ServerLauncher {
     public static void main(String[] args) {
-        if (args.length >= 1) {
-            int portNumber = Integer.parseInt(args[0]);
-            if (portNumber >= 0 && portNumber <= 65535) {
-                Server server = new Server(4342);
-                server.run();
-            }
+        if (args.length < 1) {
+            System.out.println("Please start with a port number");
+            System.out.println("like \"java -jar filename 4342\"");
         }
-        System.out.println("Please start with a port number");
-        System.out.println("like \"java -jar filename 4342\"");
+        int portNumber;
+        try {
+            portNumber = Integer.parseInt(args[0]);
+            if (portNumber >= 0 && portNumber <= 65535) {
+                throw new NumberFormatException();
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Port number is not valid");
+            return;
+        }
+
+        /*
+        TCPServer sever = new TCPServer.Builder()
+                .withHandler(new ServerThread(null, new DataBase<String, String>()))
+                .
+                */
+        Server server = new Server(portNumber);
+        server.run();
     }
 }
